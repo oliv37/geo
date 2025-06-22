c60dc599d8a1d56a6ac20480dd460c6ae26e277d
oliv37 <olivier.boisse@laposte.net>
2025-06-22 21:02:38 +0200

    refactor: push animation

--- src/app/components/exercice/exercice-progress-bar/exercice-progress-bar.ts
+++ src/app/components/exercice/exercice-progress-bar/exercice-progress-bar.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, effect, input, signal } from '@angular/core';
 
 @Component({
   selector: 'app-exercice-progress-bar',
@@ -7,6 +7,13 @@ import { Component, input } from '@angular/core';
 export class ExerciceProgressBar {
   progressPercent = input.required<number>();
   className = input<string>(
-    'h-1 bg-(--color-exercice) transition-all duration-500'
+    'h-1 bg-(--color-exercice) transition-all duration-700'
   );
+
+  progressPercentDeffered = signal<number>(0);
+
+  progressPercentEffect = effect(() => {
+    const progressPercent = this.progressPercent();
+    setTimeout(() => this.progressPercentDeffered.set(progressPercent));
+  });
 }
